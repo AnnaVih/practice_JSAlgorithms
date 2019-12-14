bee6385ae096f6836ac84f4fe13f8de44a3308b1
Anna Vihrogonova <anna-kobzii@mail.ru>
2019-12-14 22:54:27 +0000

    Add another alternative solution to anagram problem

--- DataStructures_Algorithms/anagrams/index.js
+++ DataStructures_Algorithms/anagrams/index.js
@@ -8,31 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA)
+//   const bCharMap = buildCharMap(stringB)
+//   if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+//     return false
+//   }
 
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
+//   for (let char in aCharMap){
+//     if(aCharMap[char] !== bCharMap[char]){
+//       return false
+//     }
+//   }
 
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
+//   return true
+// }
 
-  return true;
+// function buildCharMap(str) {
+//   var charMap = {}
+//   for(let char of str.replace(/[^\w]/g, '').toLowerCase()){
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+//   return charMap
+// }
+
+const anagrams = (stringA, stringB) => {
+  return cleanString(stringA) === cleanString(stringB)
 }
 
-function buildCharMap(str) {
-  const charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-
-  return charMap;
+const cleanString = str => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
 }
 
 module.exports = anagrams;
