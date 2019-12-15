8dd043074e6e0d19016949c41335a505867ed474
Anna Vihrogonova <anna-kobzii@mail.ru>
2019-12-15 23:27:40 +0000

    write steps solution using recursive function

--- DataStructures_Algorithms/steps/index.js
+++ DataStructures_Algorithms/steps/index.js
@@ -17,19 +17,38 @@
 //       '### '
 //       '####'
 
-const steps = n => {
-  for (let row = 0; row < n; row++){
-    let stair = ''
-    for(let column = 0; column < n; column++){
-      if(column <= row){
-        stair += '#'
-      } else {
-        stair += ' '
-      }
-    }
+// const steps = n => {
+//   for (let row = 0; row < n; row++){
+//     let stair = ''
+//     for(let column = 0; column < n; column++){
+//       if(column <= row){
+//         stair += '#'
+//       } else {
+//         stair += ' '
+//       }
+//     }
 
-    console.log(stair)
+//     console.log(stair)
+//   }
+// }
+
+const steps = (n, row = 0, stair = '') => {
+  if(n === row){
+    return
   }
+
+  if(n === stair.length){
+    console.log(stair)
+    steps(n, row + 1)
+    return
+  }
+
+  if(stair.length <= row){
+    stair += '#'
+  } else {
+    stair += ' '
+  }
+  steps(n, row, stair)
 }
 
 module.exports = steps;
