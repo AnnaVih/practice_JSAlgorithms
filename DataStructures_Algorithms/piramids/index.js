7c0ba44c412620281ae8f88c3b0e49e0dacfd34e
Anna Vihrogonova <anna-kobzii@mail.ru>
2019-12-17 22:52:13 +0000

    write first solution for piramids

--- DataStructures_Algorithms/piramids/index.js
+++ DataStructures_Algorithms/piramids/index.js
@@ -14,6 +14,19 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+const pyramid = n => {
+  const midpoint = Math.floor((2 * n - 1) / 2)
+  for (let row = 0; row < n; row++){
+    let level = ''
+    for(let column = 0; column < 2 * n - 1; column++){
+      if(midpoint - row <= column && midpoint + row >= column){
+        level += '#'
+      } else {
+        level += ' '
+      }
+    }
+    console.log(level)
+  }
+}
 
 module.exports = pyramid;
