3a2d508bbbc274547a9705b0e04f2936668a705e
Anna Vihrogonova <anna-kobzii@mail.ru>
2019-12-22 18:06:34 +0000

    write 2 algorithm solutions for vowels problem

---
+++ DataStructures_Algorithms/vowels/index.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+// const vowels = str  => {
+//   let count = 0
+//   let checker = ['a','e','i','o','u']
+//   for(let char of str.toLowerCase()){
+//     if(checker.includes(char)){
+//       count++
+//     }
+//   }
+//   return counter
+// }
+
+const vowels = str => {
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
+
+module.exports = vowels;
