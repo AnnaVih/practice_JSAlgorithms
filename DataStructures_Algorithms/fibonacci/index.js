84b69ad8563ed7650a34affc963bacf9b26b3c83
Anna Vihrogonova <anna-kobzii@mail.ru>
2019-12-27 18:50:46 +0000

    write first solution algorithm for fibonacci numbers

---
+++ DataStructures_Algorithms/fibonacci/index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+const fib = n => {
+  const result = [0, 1]
+  for(let i = 2; i <= n; i++){
+    const a = result[i - 1]
+    const b = result[i - 2]
+    result.push(a + b)
+  }
+  return result[n]
+}
+
+module.exports = fib;
