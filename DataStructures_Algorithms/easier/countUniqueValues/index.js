2fea873c6386e01ea4721d8f7b1812e61d4aaca0
AnnaVih <anna-kobzii@mail.ru>
2020-03-14 18:02:25 +0000

    write countUniqueValues into arr of numbers

---
+++ DataStructures_Algorithms/easier/countUniqueValues/index.js
@@ -0,0 +1,15 @@
+// USAGE POINTERS PATTERN
+
+const countUniqueValues = arr => {
+    if (arr.length === 0) return 0
+    let i = 0
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1
+}
+
+console.log(countUniqueValues([1, 1, 1, 3, 4, 5, 5])) // Should return 4
