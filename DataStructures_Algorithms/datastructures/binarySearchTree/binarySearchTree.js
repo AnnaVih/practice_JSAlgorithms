055d89e55b99d729b458a55760c25e75d6c6a8c1
AnnaVih <anna-kobzii@mail.ru>
2020-03-30 22:59:34 +0100

    write dfs and bfs traverse methods

--- DataStructures_Algorithms/datastructures/binarySearchTree/binarySearchTree.js
+++ DataStructures_Algorithms/datastructures/binarySearchTree/binarySearchTree.js
@@ -53,6 +53,52 @@ class BinarySearchTree {
         }
         return false
     }
+
+    BFS() {
+        let data = []
+        let queue = []
+        let node
+        queue.push(this.root)
+        while (queue.length) {
+            node = queue.shift()
+            data.push(node)
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+        return data
+    }
+
+    DFSPreOrder() {
+        let data = []
+        const traverse = node => {
+            data.push(node.value)
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data
+    }
+    DFSPostOrder() {
+        let data = []
+        const traverse = node => {
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+            data.push(node.value)
+        }
+        traverse(this.root)
+        return data
+    }
+    DFSInOrder() {
+        let data = []
+        const traverse = node => {
+            if (node.left) traverse(node.left)
+            data.push(node.value)
+            if (node.right) traverse(node.right)
+            data.push(node.value)
+        }
+        traverse(this.root)
+        return data
+    }
 }
 
 let tree = new BinarySearchTree()
